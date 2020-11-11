e30c5d3e542a8da179e2698a6c5caaf545f9c196
Akampa Darlington <darlingtonakampa@gmail.com>
2020-11-11 18:57:29 -0500

    updating scripts

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from './ErrorBoundry';
 import SearchBox from '../components/SearchBox';
 import '../App.css';
 
@@ -38,7 +39,9 @@ class App extends Component {
 				<h1 className='f1'>Robofriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<Scroll>
-					<CardList robots={filteredRobots}/>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundry>
 				</Scroll>
 			</div>
 		);
